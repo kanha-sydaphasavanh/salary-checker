8cfbb47af14fd9f51a9a057e0049d2e936a43b24
ksydaphasavanh <k.sydaphasavanh@laposte.net>
2025-07-31 23:07:01 +0200

    refactor app.js to use ES6 imports, enhance file handling, and integrate WebDAV for file uploads

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios')
-// require('dotenv').config();
+import { existsSync, readFileSync, writeFileSync, createWriteStream } from 'fs';
+import { resolve as _resolve } from 'path';
+import axios from 'axios';
+import { AuthType, createClient } from 'webdav';
+import 'dotenv/config'; // Import dotenv to load environment variables
 
 const now = new Date();
 const DATE = now.toLocaleDateString('fr-FR', { timeZone: 'Europe/Paris' });
 const TIME = now.toLocaleTimeString('fr-FR', { timeZone: 'Europe/Paris' });
 const DATETIME = `${DATE} ${TIME}`;
 const AUTH_TOKEN = process.env.ILUCCA_AUTH_TOKEN;
+const __dirname = _resolve()
 
 const api_ilucca = axios.create({
     baseURL: 'https://sivecogroup.ilucca.net/payslip/api/payslips',
@@ -26,11 +28,17 @@ const api_discord = axios.create({
     }
 })
 
-const STATE_FILE = path.resolve(__dirname, 'state.json');
-function loadState() {
+const webdavClient = createClient(process.env.NEXTCLOUD_WEBDAV_BASE_URL, {
+    authType: AuthType.BASIC,
+    username: process.env.NEXTCLOUD_ADMIN_USER,
+    password: process.env.NEXTCLOUD_ADMIN_PASSWORD,
+});
+
+const STATE_FILE = _resolve(__dirname, 'state.json');
+const loadState = () => {
     try {
-        if (fs.existsSync(STATE_FILE)) {
-            const data = fs.readFileSync(STATE_FILE, 'utf8').trim();
+        if (existsSync(STATE_FILE)) {
+            const data = readFileSync(STATE_FILE, 'utf8').trim();
             if (!data) {
                 // Fichier vide
                 return { lastLength: 0 };
@@ -44,9 +52,9 @@ function loadState() {
     return { lastLength: 0 };
 }
 
-function saveState(state) {
+const saveState = (state) => {
     try {
-        fs.writeFileSync(STATE_FILE, JSON.stringify(state, null, 2), 'utf8');
+        writeFileSync(STATE_FILE, JSON.stringify(state, null, 2), 'utf8');
     } catch (err) {
         console.error('Erreur écriture fichier état :', err);
     }
@@ -66,64 +74,76 @@ let state = loadState();
         console.log(`${DATETIME} - Nombre de fiches de paie : ${items.length}`);
 
         if (items.length > state.lastLength) {
-            
+
             const firstId = items[0].id;
             const payload = {
                 ids: [firstId],
                 token: '',
             };
 
-            const downloadResponse = await api_ilucca.post(`/download`,
-                payload,
-                {
-                    responseType: 'stream',
-                }
-            );
+            const downloadResponse = await api_ilucca.post(`/download`, payload, { responseType: 'stream', });
 
             if (downloadResponse.status !== 200) {
                 console.error(`${DATETIME} - Erreur lors du téléchargement :`, downloadResponse.statusText);
                 return;
             }
-            const fileName = downloadResponse.headers['content-disposition'].match(/filename="(.+?)"/)[1];
-            const filePath = path.resolve(__dirname, 'data', fileName);
-            const writer = fs.createWriteStream(filePath);
-            saveState({ lastLength: items.length });
-            
-            console.log(`${DATETIME} - Nouvelle fiche détectée, id : ${firstId} - ${fileName}`);
-            console.log(`${DATETIME} - Fichier téléchargé et sauvegardé : ${filePath}`);
-            downloadResponse.data.pipe(writer);
 
-            if(process.env.ALLOW_DISCORD_NOTIFICATIONS) {
+            let fileName = downloadResponse.headers['content-disposition'].match(/filename="(.+?)"/)[1];
+            fileName = formatFilename(fileName);
+
+            saveState({ lastLength: items.length });
+            // const writer = createWriteStream(filePath);
+
+            try {
+                // convert into a buffer
+                const chunks = [];
+                for await (const chunk of downloadResponse.data) {
+                    chunks.push(chunk);
+                }
+                const buffer = Buffer.concat(chunks);
+                const filePath = `${process.env.NEXTCLOUD_WEBDAV_PATH_TARGET}/${fileName}`;
+                await webdavClient.putFileContents(filePath, buffer);
+
+                console.log(`${DATETIME} - Nouvelle fiche détectée, id : ${firstId} - ${fileName}`);
+                console.log(`${DATETIME} - Fichier téléchargé et sauvegardé : ${filePath}`);
+            } catch (error) {
+                console.error(`${DATETIME} - Erreur lors de l'envoi vers WebDAV :`, error);
+            }
+
+            const ALLOW_DISCORD = process.env.ALLOW_DISCORD_NOTIFICATIONS === "true";
+
+            if (ALLOW_DISCORD) {
                 api_discord.post(`/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
                     content: `\u{1F4B8}\u{1F4B8} - Nouvelle fiche de paie arrivé : ${fileName}.`,
                 });
             }
 
-            await new Promise((resolve, reject) => {
-                writer.on('finish', resolve);
-                writer.on('error', reject);
-            });
+            // await new Promise((resolve, reject) => {
+            //     writer.on('finish', resolve);
+            //     writer.on('error', reject);
+            // });
 
-            // Suppression du fichier state.json
-            // try {
-            //     if (fs.existsSync(STATE_FILE)) {
-            //         fs.unlinkSync(STATE_FILE);
-            //         console.log('Fichier state.json supprimé.');
-            //     }
-            // } catch (err) {
-            //     console.error('Erreur lors de la suppression du fichier state.json :', err);
-            // }
 
             // Reset state en mémoire
             state = { lastLength: 0 };
         } else {
-
             console.log(`${DATETIME} - Pas de nouvelle fiche de paie.`);
         }
     } catch (error) {
-        console.error(`${DATETIME} - Erreur lors de la vérification des fiches de paie :`, error.message);
+        console.error(`${DATETIME} - Erreur lors de la vérification des fiches de paie :`, error.stack || error.message || error);
     }
 }
 )().catch(err => {
-    console.error(`${DATETIME} - Erreur inattendue :`, err.message);
+    console.error(`${DATETIME} - Erreur inattendue :`, err.stack || err.message || err);
 });
+
+const formatFilename = (filename) => {
+    const match = filename.match(/^(\d{4}-\d{2}) - ([^-]+) - [^-]+ - ([a-zéûîôàèùç]+ \d{4})\.pdf$/i);
+    if (match) {
+        const [, yearMonth, name, monthYear] = match;
+        // Capitalize the first letter of the month
+        const formattedMonthYear = monthYear.charAt(0).toUpperCase() + monthYear.slice(1);
+        return `${yearMonth} - ${name} - ${formattedMonthYear}.pdf`;
+    }
+    return filename;
+}
