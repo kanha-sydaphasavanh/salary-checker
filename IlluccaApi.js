76f507cd2602235f5a9f153b970cd0895a8a91e8
Kanha Sydaphasavanh <k.sydaphasavanh@laposte.net>
2025-08-01 09:47:07 +0200

    IluccaApi class

--- IlluccaApi.js
+++ IlluccaApi.js
@@ -1,3 +1,39 @@
+import axios from "axios";
+import 'dotenv/config';
+
 export class IluccaApi {
-    
+    constructor() {
+        this.now = new Date();
+        this.AUTH_TOKEN = process.env.ILUCCA_AUTH_TOKEN;
+        this.api = axios.create({
+            baseURL: 'https://sivecogroup.ilucca.net/payslip/api/payslips',
+            headers: {
+                'Cookie': `_BEAMER_USER_ID_xWDIXXVd32349=${process.env.ILUCCA_BEAMER_USER_ID}; _BEAMER_FIRST_VISIT_xWDIXXVd32349=${this.now.toISOString()}; authToken=${this.AUTH_TOKEN}; _dd_s=rum=0`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+    }
+
+    async getPayslips() {
+        try {
+            const response = await this.api.get('/mine?limit=1000');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching payslips:', error || error.message);
+            throw error;
+        }
+    }
+
+    async downloadPayslip(payload) {
+        try {
+            const response = await this.api.post(`/download`, payload, {
+                responseType: 'stream'
+            });
+            return response;
+        } catch (error) {
+            console.error('Error downloading payslip:', error || error.message);
+            throw error;
+        }
+    }
 }
